b1b5fd51f1901ea7b30b107610c9061358eb5a46
pradoh97 <pradoh97@gmail.com>
2021-10-19 01:32:44 -0300

    Add files via upload

---
+++ portafolio.js
@@ -0,0 +1,39 @@
+let proyecto;
+let url;
+
+url['codigo'] = 'https://github.com/pradoh97/';
+url['live'] = 'https://pradoh97.github.io/';
+url['imagenes'] = 'img/';
+
+proyecto['reproductor'] = {
+  'repositorio': 'reproductor-de-musica-web/',
+  'portada': 'reproductor.png'
+};
+proyecto['taskly'] = {
+  'repositorio': 'taskly/',
+  'portada': 'taskly.png'
+};
+proyecto['galeria'] = {
+  'repositorio': 'img-slider/',
+  'portada': 'galería.png'
+};
+proyecto['tarjetas'] = {
+  'repositorio': 'A-bit-of-CSS-and-HTML/Sesion%202%20-%20tarjetas/Mercado%20Libre/',
+  'portada': 'tarjetas.png'
+};
+proyecto['carrito'] = {
+  'repositorio': 'cursos-pagos/',
+  'portada': 'carrito.png'
+};
+proyecto['opencourse'] = {
+  'repositorio': 'opencourse/',
+  'portada': 'opencourse.png'
+};
+proyecto['planes'] = {
+  'repositorio': 'frontend-challenge-3/',
+  'portada': 'planes.png'
+};
+proyecto['wrox'] = {
+  'repositorio': 'wrox-remake/',
+  'portada': 'wrox.png'
+};
